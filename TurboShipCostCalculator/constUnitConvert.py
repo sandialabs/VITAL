b8920437ce6edd38e9844897a5b20c0de1aa0b27
akeow <91087927+akeow@users.noreply.github.com>
2024-07-08 12:06:32 -0600

    Minor update for readability constUnitConvert.py

--- TurboShipCostCalculator/constUnitConvert.py
+++ TurboShipCostCalculator/constUnitConvert.py
@@ -12,24 +12,32 @@ class ConstantsUnitConversion:
     """
     A class to hold constants for various unit conversions.
     """
-    sec2days = 1/(24.0*3600.0)      # second to days
-    mile2m = 1609.34            # miles to meter
-    m2mile = 1/mile2m         	# meter to miles
-    m2km = 1e-3                 # meter to kilometer
-    W2MW = 1e-6                 # Watt to MegaWatt
-    W2kW = 1e-3                 # Watt to kiloWatt
-    kE2E = 1e3                  # kilo euro to euro
-    N2mTon = 1.019716e-4        # newton to metric tons
-    N2kN = 1e-3                 # newton to kilo newton
-    euro2dollar = 1.26*1.1304   # euro to dollar
-    ft2m = 0.3048               # feet to meter
-    cms2ms = 1e-2               # cm/s to m/s
-    rads2rpm = 60.0/(2.0*math.pi)     # rad/s to rpm
-    m32cm3 = 1e+6				# cubic meter to cubic centimeter
-    hrs2days = 1/24.0             # hours to days
+    
+    sec2days: float = 1 / (24.0 * 3600.0)  # second to days
+    mile2m: float = 1609.34                # miles to meter
+    m2mile: float = 1 / mile2m             # meter to miles
+    m2km: float = 1e-3                     # meter to kilometer
+    W2MW: float = 1e-6                     # Watt to MegaWatt
+    W2kW: float = 1e-3                     # Watt to kiloWatt
+    kE2E: float = 1e3                      # kilo euro to euro
+    N2mTon: float = 1.019716e-4            # newton to metric tons
+    N2kN: float = 1e-3                     # newton to kilo newton
+    euro2dollar: float = 1.26 * 1.1304     # euro to dollar
+    ft2m: float = 0.3048                   # feet to meter
+    cms2ms: float = 1e-2                   # cm/s to m/s
+    rads2rpm: float = 60.0 / (2.0 * math.pi)  # rad/s to rpm
+    m32cm3: float = 1e6                    # cubic meter to cubic centimeter
+    hrs2days: float = 1 / 24.0             # hours to days
 
-    def __setattr__(self, name, value):
+    def __setattr__(self, name: str, value) -> None:
         """
         Prevents reassignment of constants.
+        
+        Parameters:
+        - name (str): The name of the attribute attempting to be modified.
+        - value: The value attempting to be set.
+        
+        Raises:
+        - AttributeError: If an attempt is made to modify an attribute, indicating immutability.
         """
-        raise AttributeError(f"can't reassign constant '{name}'")
+        raise AttributeError(f"Can't reassign constant '{name}'")
